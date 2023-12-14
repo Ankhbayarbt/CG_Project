f05cc99f8e442ca7fb03bd2ebe1ff30e16999156
Ankhbayarbt <ankhaaa205@gmail.com>
2023-12-14 11:10:22 +0800

    Initial Commit

---
+++ testProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  testProject
+//
+//  Created by Ankhbayar on 2023.12.14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
